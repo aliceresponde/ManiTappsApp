d918ccbaa7caf1d5b6a28456c069f1869d903942
Alicia Beltran <aliceresponde@gmail.com>
2019-03-13 13:40:37 -0500

    order on activity, and descriptions

--- app/src/main/java/com/aliceresponde/manytappsapp/MainActivity.kt
+++ app/src/main/java/com/aliceresponde/manytappsapp/MainActivity.kt
@@ -11,7 +11,7 @@ import android.widget.Toast
 class MainActivity : AppCompatActivity() {
     private var score = 0
     private val countDownInterval: Long = 1000 // 1s
-    private var initialCountDown:Long = 60000 // 60s
+    private var initialCountDown: Long = 60000 // 60s
     private lateinit var countDownTimer: CountDownTimer
     private var gameStarted = false
 
@@ -24,49 +24,59 @@ class MainActivity : AppCompatActivity() {
         val game_time_left = findViewById<TextView>(R.id.game_time_left)
         val game_tap_me_button = findViewById<Button>(R.id.game_tap_me_button)
 
-        resetGame(game_score_text_view)
+        resetGame()
         game_tap_me_button.setOnClickListener {
-            intentScore()
+            incrementScore()
         }
     }
 
     /**
      * set score in 0 and initialize the timer as for a minut count down
      */
-    private fun resetGame(game_score_text_view: TextView) {
+    private fun resetGame() {
+        var timeLeft = initialCountDown / 1000  // time in second
         score = 0
         game_score_text_view.text = getString(R.string.game_your_score, score.toString())
-        var timeLeft = initialCountDown/1000  // time in seconds
         game_time_left.text = getString(R.string.game_time_left, timeLeft.toString())
 
-        countDownTimer = object: CountDownTimer(initialCountDown, countDownInterval){
+        countDownTimer = object : CountDownTimer(initialCountDown, countDownInterval) {
             override fun onFinish() {
-                //toast points archived and reset screen
                 endGame()
             }
 
             //update left time label
             override fun onTick(millisUntilFinished: Long) {
-             val timeLeft = millisUntilFinished/1000
+                val timeLeft = millisUntilFinished / 1000
                 game_time_left.text = getString(R.string.game_time_left, timeLeft.toString())
             }
         }
         gameStarted = false
     }
 
-    private fun endGame(){
+    /**
+     * Show score to user, and reset screen (timer 60, score 0)
+     */
+    private fun endGame() {
         Toast.makeText(this, getString(R.string.game_gae_over_message, score.toString()), Toast.LENGTH_LONG).show()
-        resetGame(game_score_text_view)
+        resetGame()
     }
-    private fun intentScore() {
-        if (!gameStarted){
+
+    /**
+     * Start game if  needed, increment score and update score
+     */
+    private fun incrementScore() {
+        if (!gameStarted) {
             startGame()
         }
+
         score += 1
         val newScoreText = getString(R.string.game_your_score, score.toString())
         game_score_text_view.text = newScoreText
     }
 
+    /**
+     * Start timer decremental update  game flag
+     */
     private fun startGame() {
         countDownTimer.start()
         gameStarted = true
